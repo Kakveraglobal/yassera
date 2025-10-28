ce0594307bf13c8a43257bea87e27ae786ebff8a
ChideraChima <chidera8564@bazeuniversity.edu.ng>
2025-10-28 18:39:41 +0100

    Update base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Yassera/', // GitHub Pages repository name
+  base: '/yassera/', // GitHub Pages repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
